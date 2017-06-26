4ef51f38368abb99f5eb8c1cfab4a632e7717a4c
Paragon Initiative Enterprises, LLC <security@paragonie.com>
2017-06-26 03:40:50 -0400

    Add local/ directory. Update composer.

--- src/settings.php
+++ src/settings.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'settings' => [
-        'displayErrorDetails' => true, // set to false in production
+        'displayErrorDetails' => false, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
         // Renderer settings
