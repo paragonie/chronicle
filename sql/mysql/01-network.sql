37f1dd7972aaef100fa80ee29346c5e155c0b69d
Paragon Initiative Enterprises <security@paragonie.com>
2017-07-01 13:29:04 -0400

    Begin branch for cross-signing and replication.

---
+++ sql/mysql/01-network.sql
@@ -0,0 +1,28 @@
+CREATE TABLE chronicle_xsign_targets (
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  name TEXT,
+  url TEXT,
+  publickey TEXT,
+  policy TEXT,
+  lastrun TEXT
+);
+
+CREATE TABLE chronicle_replication_sources (
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  name TEXT,
+  url TEXT,
+  publickey TEXT
+);
+
+CREATE TABLE chronicle_replication_chain (
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  source INT REFERENCES chronicle_replication_sources(id),
+  data TEXT,
+  prevhash TEXT,
+  currhash TEXT,
+  hashstate TEXT,
+  summaryhash TEXT,
+  publickey TEXT,
+  signature TEXT,
+  created DATETIME
+);
---
+++ sql/sqlite/01-network.sql
@@ -0,0 +1,28 @@
+CREATE TABLE chronicle_xsign_targets (
+  id INTEGER PRIMARY KEY ASC,
+  name TEXT,
+  url TEXT,
+  publickey TEXT,
+  policy TEXT,
+  lastrun TEXT
+);
+
+CREATE TABLE chronicle_replication_sources (
+  id INTEGER PRIMARY KEY ASC,
+  name TEXT,
+  url TEXT,
+  publickey TEXT
+);
+
+CREATE TABLE chronicle_replication_chain (
+  id INTEGER PRIMARY KEY ASC,
+  source INTEGER,
+  data TEXT,
+  prevhash TEXT,
+  currhash TEXT,
+  hashstate TEXT,
+  summaryhash TEXT,
+  publickey TEXT,
+  signature TEXT,
+  created DATETIME
+);
