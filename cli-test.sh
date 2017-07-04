e1fe08c43198cc3fd9e48ab230f59c2b6e3b24a3
Paragon Initiative Enterprises <security@paragonie.com>
2017-07-04 14:25:10 -0400

    Deduplicate

--- cli-test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Make sure you run `composer start` first!
-php tests/cli/start.php
-php tests/cli/commands/private-endpoints.php
-php tests/cli/commands/public-endpoints.php
